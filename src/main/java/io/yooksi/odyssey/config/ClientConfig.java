5daa593bbd0dd95cd39892cadc48acbf26dfc741
Matthew Cain <yooksi@zoho.com>
2020-05-04 09:56:24 +0200

    Write config boilerplate

---
+++ src/main/java/io/yooksi/odyssey/config/ClientConfig.java
@@ -0,0 +1,12 @@
+package io.yooksi.odyssey.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class ClientConfig {
+
+	public ClientConfig(ForgeConfigSpec.Builder builder) {
+
+		// Finish building configurations
+		builder.build();
+	}
+}
---
+++ src/main/java/io/yooksi/odyssey/config/OdysseyConfig.java
@@ -0,0 +1,35 @@
+package io.yooksi.odyssey.config;
+
+import io.yooksi.odyssey.common.Defines;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import org.apache.commons.lang3.tuple.Pair;
+
+@Mod.EventBusSubscriber(modid = Defines.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class OdysseyConfig {
+
+	public static final ClientConfig CLIENT;
+	public static final ForgeConfigSpec CLIENT_SPEC;
+
+	static {
+		final Pair<ClientConfig, ForgeConfigSpec> specPair =
+				new ForgeConfigSpec.Builder().configure(ClientConfig::new);
+
+		CLIENT_SPEC = specPair.getRight();
+		CLIENT = specPair.getLeft();
+	}
+
+	public static void bakeConfig() {
+
+	}
+
+	@SubscribeEvent
+	public static void onModConfigEvent(final ModConfig.ModConfigEvent configEvent) {
+
+		if (configEvent.getConfig().getSpec() == CLIENT_SPEC) {
+			bakeConfig();
+		}
+	}
+}
